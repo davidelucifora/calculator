5bfea117d57911b6cc5920f5ff337d5e2917c51b
Davide <davidelucifora1@gmail.com>
2021-01-18 15:28:41 +0530

    Refactor code.

--- script.js
+++ script.js
@@ -1,79 +1,76 @@
-let totalOnDisplay = '';
+const display = document.getElementById('calc-display');
+const calcArray = []
+const dotButton = document.querySelector('.dot-btn')
+const buttons = document.querySelectorAll('.grid-btn');
 let displayReset = true;
-let operator = ''
-let calcArray = []
-let calculatorDisplay = document.getElementById('calc-display');
-calculatorDisplay.innerText = 0;
+display.innerText = 0; 
+let operator
 
-let buttons = document.querySelectorAll('.grid-btn');
 buttons.forEach(btn => {
     btn.addEventListener('click', takeInput)
-})
+});
 
-//Takes Input from buttons
+//Takes Input from buttons and updates the display
 function takeInput(e){
+const buttonPressed = e.target
 
-    if(e.target.classList.contains('num-btn')){
-        if (displayReset === true) {
-                totalOnDisplay = '';
-                displayReset = false
+switch ( true ){
+    case buttonPressed.classList.contains('num-btn'):
+        if (displayReset) {
+            display.innerText = '';
+            displayReset = false
         }
-        
-        totalOnDisplay += e.target.innerHTML
-        calculatorDisplay.innerHTML = totalOnDisplay
-    }
 
-    if (e.target.classList.contains('dot-btn'))
-    {
-        totalOnDisplay += e.target.innerHTML
-        calculatorDisplay.innerHTML = totalOnDisplay
-        e.target.disabled = true;
-        e.target.classList.add('btn-disabled')
-    }
-    if(e.target.classList.contains('delete-btn')){
-        totalOnDisplay = '0'
-        calculatorDisplay.innerText = totalOnDisplay
-        calcArray = []
-        displayReset = true;
-        document.querySelector('.dot-btn').disabled = false;
-    }
-    if(e.target.classList.contains('calc-btn')){
+        display.innerText += buttonPressed.innerText
+    break;
+
+    case buttonPressed.classList.contains('dot-btn'):
+            display.innerText += buttonPressed.innerText
+            disableDotButton(dotButton)
+    break;
+    
+    case buttonPressed.classList.contains('delete-btn'):
+            display.innerText = '0'
+            calcArray.length = 0
+            displayReset = true;
+            enableDotButton(dotButton)
+    break;
+    
+    case buttonPressed.classList.contains('calc-btn'):   
         displayReset = true;
 
         if (!calcArray.length){
-            operator = e.target.innerHTML
-            calcArray.push(parseFloat(calculatorDisplay.innerText))
-            console.log('array was empty', calcArray,operator)
-            document.querySelector('.dot-btn').disabled = false;
+            operator = buttonPressed.innerText
+            calcArray.push(parseFloat(display.innerText))
+            enableDotButton(dotButton)
             displayReset === true;
         }
         else {
-            operator === '' ? operator = e.target.innerHTML : ''
-            calculatorDisplay.innerHTML = operate(operator)
-            operator = e.target.innerHTML
+            operator === '' ? operator = buttonPressed.innerText : ''
+            display.innerText = operate(operator)
+            operator = buttonPressed.innerText
         }
-        console.log(calcArray)
-
-}
-    if (e.target.classList.contains('equal-btn')){
+    break;
+    
+    case buttonPressed.classList.contains('equal-btn'):
+        if (display.innerText.includes('.')) disableDotButton(dotButton)    
         if (operator === ''){
-            calculatorDisplay.innerHTML += ''
+            display.innerText += ''
         }
         else {
-        calcArray.push(parseFloat(calculatorDisplay.innerText))
-        calculatorDisplay.innerHTML = operate(operator)
-        displayReset === true;
-        operator = ''
-        document.querySelector('.dot-btn').disabled = false;
-    }
-    }
-    if (e.target.classList.contains('negative-btn')){
-        calcArray.push(parseFloat(calculatorDisplay.innerText))
-        operator = e.target.innerHTML;
-        calculatorDisplay.innerHTML = operate(operator)
-    }
+            calcArray.push(parseFloat(display.innerText))
+            display.innerText = operate(operator)
+            displayReset === true;
+            operator = ''
+        }
+    break;
 
-    console.log(calcArray)
+    case buttonPressed.classList.contains('negative-btn'):
+        calcArray.push(parseFloat(display.innerText))
+        operator = buttonPressed.innerText;
+        display.innerText = operate(operator)
+    break;
+    }
 }
 
 // Operation Functions
@@ -84,46 +81,48 @@ function operate(operator){
     switch (operator) {
         case '+':
             result = sum(calcArray);
-            calcArray = []
+            calcArray.length = 0
             calcArray.push(result)
             return result
-            break
+            
         case '-':
             result = subtract(calcArray);
-            calcArray = []
+            calcArray.length = 0
             calcArray.push(result)
             return result
-            break
+            
         case 'x':
             result = multiply(calcArray);
-            calcArray = []
+            calcArray.length = 0
             calcArray.push(result)
             return result
-            break
+            
         case '÷':
-            if (calcArray.some(num => num = 0)) return 'Noope!'
+            if (calcArray.includes(0)) {
+                displayReset = true;
+                return 'Noope!'}
             result = divide(calcArray)
-            calcArray = []
+            calcArray.length = 0
             calcArray.push(result)
             return result
-            break
+            
         case '%':
             result = percentage(calcArray);
-            calcArray = []
+            calcArray.length = 0
             calcArray.push(result)
             return result
-            break 
+             
         case '+/-':
             result = negative(calcArray);
-            calcArray = []
+            calcArray.length = 0
             calcArray.push(result)
             return result
-            break 
+            
     }
 
 }
 
-//Sub Operations
+//Arithmetic Operations
 function sum(calcArray){
     return  calcArray.reduce((a,b) => a + b,)
 }
@@ -140,23 +139,19 @@ function percentage(calcArray){
     return calcArray.reduce((a,b) => (a * b) / 100);
 }
 function negative(calcArray){
-    return -Math.abs(calcArray[0])
+    return -Math.abs(display.innerText)
 }
 
+// Disable/Enable Dot button
+function disableDotButton(dotButton){
 
+        dotButton.classList.add('btn-disabled')
+        dotButton.disable = true;
+}
+function enableDotButton(dotButton){
 
+    dotButton.classList.remove('btn-disabled')
+    dotButton.disable = false;
+}
 
-/*
-I hit +
-Take the number from the display and add it to the array.
-if it's the only number in the Array, do nothing, store the + as operator for the next
-I type another number
-I hit + again.
-I check the array, there's something inside already...
-
-I take the operator that was stored before, add this number to what is in the array with that operator
-take the result and store it alone on the array.
-
-store the new operator for the next operation
-Rinse and repeat
-*/
+//
